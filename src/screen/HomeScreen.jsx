bffd1b8822d9edfe5f2e02b9af912b868d5edc41
Matthew Beale <129032908+mttble@users.noreply.github.com>
2024-04-03 13:43:55 +1030

    added react-linear-gradient to HomeScreen

--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -2,17 +2,24 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import AntDesign from 'react-native-vector-icons/AntDesign'
+import LinearGradient from 'react-native-linear-gradient'
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Icon name="facebook" size={25} />
-      <AntDesign name="arrowright" size={50} color={"red"} />
-    </View>
+    <LinearGradient
+        colors={['#FDF0F3','#FFFBFC']}
+        style={styles.container}>
+        <Text style={styles.buttonText}>
+            Sign in with Facebook
+        </Text>
+    </LinearGradient>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+})
